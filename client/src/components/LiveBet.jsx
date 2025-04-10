448714636e5fc402d7c0938c8c38f25d11fe9ecd
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-04-10 16:57:50 +0530

    getting bets

--- client/src/components/LiveBet.jsx
+++ client/src/components/LiveBet.jsx
@@ -36,7 +36,7 @@ function LiveBet() {
     const fetchQuestions = async () => {
       try {
         const contractQuestions = await publicClient.readContract({
-          address: "0x5a8E771b5D0B3d2e4d218478CB7C9029d00c4e5a",
+          address: "0xf12F7584143D17169905D7954D3DEab8942a310d",
           abi: wagmiAbi,
           functionName: "getAllQuestions"
         });
